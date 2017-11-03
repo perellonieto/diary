8f372879bae32aa1d6ad8973475069c1a94369d8
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-11-03 11:41:50 +0000

    Added filter by year and improved visualisation

--- style/functions.js
+++ style/functions.js
@@ -4,7 +4,7 @@ function populateToC()
 
   var newLine, el, title, link;
 
-  $(".filename").each(function() {
+  $("h1").each(function() {
     el = $(this);
     title = el.text();
     link = "#" + el.attr("id");
@@ -15,8 +15,47 @@ function populateToC()
   });
 }
 
+function showToC()
+{
+    /*
+     * Shows table of contents
+     */
+    $(".table-of-contents ul").css("display", "initial");
+}
+
+function hideToC()
+{
+    /*
+     * Hides table of contents
+     */
+    $(".table-of-contents ul").css("display", "none");
+}
+
+function refresh_interval()
+{
+  /*
+   * If argument GET refresh is set, it refreshes the page every n seconds,
+   * where n is the passed value
+   */
+  var $_GET = {};
+
+  document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+    function decode(s) {
+      return decodeURIComponent(s.split("+").join(" "));
+    }
+
+    $_GET[decode(arguments[1])] = decode(arguments[2]);
+  });
+
+  document.write($_GET['refresh']);
+  //alert($_GET['refresh']);
+
+//<meta http-equiv="refresh" content="5"> == $0
+}
+
 function init() {
   populateToC();
+  //refresh_interval();
 }
 
 window.onload = init();
